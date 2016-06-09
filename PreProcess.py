4d1a8e3c3ef247fe56191befb23036859be643d4
denffer <denffer18@gmail.com>
2016-06-09 21:30:43 +0800

    Divide.py -> PreProcess.py -> Predict.py -> Evaluate.py

--- PreProcess.py
+++ PreProcess.py
@@ -1,123 +1,154 @@
 import os
 from os import listdir
 from os.path import isfile, join
-import random
-import shutil
 import sys
+import re
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
 class PreProcess:
+
     def __init__(self):
-        """ """
-        self.src = './data/train/'
-        self.src_label = './data/train_labels.csv'
-        self.dst1 = './data/new_test/'
-        self.dst2 = './data/new_train/'
+        """ initialize the path of all the folder and files to be used """
 
-        self.dst3 = './data/new_test_labels.csv'
-        self.dst4 = './data/new_train_labels.csv'
+        print '-'*60
+        self.train_folder = './data/new_train/'
+        self.test_folder = './data/new_test/'
+        self.train_clean_folder = './data/new_train_clean/'
+        self.test_clean_folder = './data/new_test_clean/'
 
-        self.test_num = 18000
+        self.mood_file = './data/moods_mapping.txt'
 
     def create_dir(self):
         """ create folder if not found """
-        dir1 = os.path.dirname(self.dst1)
-        dir2 = os.path.dirname(self.dst2)
+        dir1 = os.path.dirname(self.train_clean_folder)
+        dir2 = os.path.dirname(self.test_clean_folder)
 
         if not os.path.exists(dir1):
             os.makedirs(dir1)
         if not os.path.exists(dir2):
             os.makedirs(dir2)
 
-    def split(self):
+    def get_training_data(self):
+        """ get training data and return a list, in which an item is the content of a document """
 
-        self.create_dir()
-        print '-'*60
-        print "Loading files from", self.src
-        src_files = []
+        print 'Loading training data from ', self.train_folder, '...'
+        train_index = []
+        train_raw = []
         cnt = 0
-        for fname in os.listdir(self.src):
-            filepath = join(self.src, fname)
-            if isfile(filepath):
+
+        for f in listdir(self.train_folder):
+            file_path = join(self.train_folder, f)
+            if isfile(file_path):
                 cnt += 1
-                src_files.append(fname)
+                if cnt % 10000 == 0:
+                    print 'finished:', cnt # line counter
+                train_index.append(f[:-4])
+                with open(file_path, 'rb') as f:
+                    train_raw.append( f.read() )
 
-            sys.stdout.write("\rStatus: %s"%(cnt))
-            sys.stdout.flush()
+        return train_index, train_raw
 
-        print "\nRandomly picking files from", self.src
-        dst_files = random.sample(src_files, self.test_num)
-        #print dst_files
+    def get_testing_data(self):
+        """ get testing data and return a list, in which an item is the content of a document """
 
-        print "Putting randomed files into", self.dst1, "and", self.dst2
+        print 'Loading testing data from ', self.test_folder, '...'
+        test_index = []
+        test_raw = []
         cnt = 0
-        for filename in src_files:
-            cnt += 1
-            if filename in dst_files:
-                shutil.copy(os.path.join(self.src, filename), self.dst1)
-            else:
-                shutil.copy(os.path.join(self.src, filename), self.dst2)
 
-            sys.stdout.write("\rStatus: %s"%(cnt))
+        for f in listdir(self.test_folder):
+            file_path = join(self.test_folder, f)
+            if isfile(file_path):
+                cnt += 1
+                if cnt % 10000 == 0:
+                    print 'finished:', cnt # line counter
+                test_index.append(f[:-4])
+                with open(file_path, 'rb') as f:
+                    test_raw.append( f.read() )
+
+        return test_index, test_raw
+
+    def get_moods(self):
+        """ get mood_mappings.txt and return a list of moods """
+        moods = []
+        print 'Loading', self.mood_file
+
+        with open(self.mood_file, 'rb') as f:
+            f.next() # skip header line
+            for line in f:
+                index, mood = line.rstrip('\n').split(',')
+                moods.append(mood)
+
+        return moods
+
+    def clean(self,raw, moods):
+        """ remove whatever crap it is in the text """
+
+        text_list = []
+        stemmer = SnowballStemmer("english")
+        stop_words = stopwords.words('english')
+
+        print "Cleaning ..."
+        cnt = 0
+        for text in raw:
+            cnt += 1
+            text = text.lower()
+            text = re.sub(r'<.*?>',' ', text)
+            text = re.sub(r'-|_', '', text)
+            text = re.sub(r'[^\w\s]','',text)
+            text = re.sub(r'\n', ' ', text)
+            text = re.sub(r'\s\s+', ' ', text)
+            #text = [word for word in text.split() if word not in stopwords.words("english")]
+            for mood in moods:
+                text = re.sub(r'%s' %(mood), (" "+mood+" ")*5, text)
+            text = " ".join([stemmer.stem(word) for word in text.split(" ")])
+            text = " ".join([i for i in text.split() if i not in stop_words])
+            #print text
+
+            text_list.append(text)
+
+            sys.stdout.write('\rStatus: %s' %(cnt))
             sys.stdout.flush()
 
-        #print dst_files
-        return dst_files
+        print ""#,text_list
+        return text_list
 
     def render(self):
+        """ render files and put them in folder 'train_clean' & 'test_clean' """
 
-        dst_files = self.split()
-        dst_indexes = [ f[:-4] for f in dst_files ]
-        #print dst_indexes
-	label = {}
-        index_list = []
-        answer_list = []
+        self.create_dir()
+        moods = self.get_moods()
 
-        print "\nLoading data from", self.src_label
-        #lines = sum(1 for line in open(self.src_label))
+        train_index, train_raw = self.get_training_data()
+        test_index, test_raw = self.get_testing_data()
+        train_clean = self.clean(train_raw, moods)
+        test_clean = self.clean(test_raw, moods)
 
-        f = open(self.src_label, 'rb')
-        cnt = 0
-        for line in f:
-            cnt += 1
-            index, answer = line.rstrip('\n').split(',')
-            index_list.append(index)
-            answer_list.append(answer)
-
-            sys.stdout.write("\rStatus: %s"%(cnt))
-            sys.stdout.flush()
-
-        index_list = index_list[1:]
-        answer_list = answer_list[1:]
-
-        print "\nMatching indexes and writing 'index, ground truth' into", self.dst3, "&", self.dst4
-
-        f1 = open(self.dst3, "w") # './data/new_test_labels.csv'
-        f2 = open(self.dst4, "w") # './data/new_train_labels.csv'
-        row1 = ["file_index,ground_truth"]
-        row2 = ["file_index,ground_truth"]
-
-        for i in xrange(len(index_list)):
-            if index_list[i] in dst_indexes:
-                row1.append(index_list[i] + "," + answer_list[i])
-            else:
-                row2.append(index_list[i] + "," + answer_list[i])
-                #row2.append("%s,%s") % (index_list[i], answer_list[i])
-                #f2.write("file_index","ground_truth")
-                #f2.write(str(index_list[i]) + "," + str(answer_list[i]) + "\n")
+        print "Putting files into", self.train_clean_folder
+        for i in xrange(len(train_index)):
+            f_name = train_index[i] + ".txt"
+            f_path = self.train_clean_folder + f_name
+            f1 = open(f_path, "w")
+            f1.write('%s\n' %(train_clean[i]))
+            f1.close()
 
             sys.stdout.write("\rStatus: %s"%(i+1))
             sys.stdout.flush()
 
-        f1.write('%s\n' %  ('\n'.join(row1)))
-        f2.write('%s\n' %  ('\n'.join(row2)))
+        print "\nPutting files into", self.test_clean_folder
+        for j in xrange(len(test_index)):
+            f_name = test_index[j] + ".txt"
+            f_path = self.test_clean_folder + f_name
+            f2 = open(f_path, "w")
+            f2.write('%s\n' %(test_clean[j]))
+            f2.close()
 
-        f1.close()
-        f2.close()
+            sys.stdout.write("\rStatus: %s"%(j+1))
+            sys.stdout.flush()
 
         print ""
 
 if __name__ == '__main__':
     p = PreProcess()
     p.render()
-
-
--- clear.sh
+++ clear.sh
@@ -1,4 +1,6 @@
-rm -r ./data/new_test
 rm -r ./data/new_train
+rm -r ./data/new_test
+rm -r ./data/new_train_clean
+rm -r ./data/new_test_clean
 rm ./data/new_test_labels.csv
 rm ./data/new_train_labels.csv
